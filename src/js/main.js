2a6e03c88c36c2d21f6fd9a69b4a9029176ebe4d
Uri <stranger354999@gmail.com>
2021-10-02 12:35:23 +0600

    v1.3b

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,4 @@
 //=require ../../node_modules/jquery/dist/jquery.js
 //=require ../../node_modules/bootstrap/dist/js/bootstrap.js
 
-const foo = () =>{
-    console.log(fgfg);
-    console.log(rt);
-}
 
